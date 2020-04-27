6fcdfe46a7229564ce4e2a91d518df7daa885fa0
bellossimo <bellossimo@naver.com>
2020-04-27 23:52:26 +0900

    Chapter 18 - 기본 SwiftUI 프로젝트 분석

---
+++ ProjectDemo/ProjectDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectDemo
+//
+//  Created by bellossimo on 2020/04/27.
+//  Copyright © 2020 bellossimo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

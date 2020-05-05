6f9b16ad95fa1694cb64080b64041739014ec6bb
bellossimo <bellossimo@naver.com>
2020-05-05 22:01:47 +0900

    Chapter 22 - SwiftUI 예제 튜토리얼

---
+++ SwiftUITutorial/SwiftUITutorial/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  SwiftUITutorial
+//
+//  Created by bellossimo on 2020/05/05.
+//  Copyright © 2020 bellossimo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var colors: [Color] = [.black, .red, .green, .blue]
+    var colornames = ["Black", "Red", "Green", "Blue"]
+    
+    @State private var colorIndex = 0
+    @State private var rotation: Double = 0
+    @State private var text: String = "Welcome to SwiftUI"
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Hello, World!")
+                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                .fontWeight(.heavy)
+                .rotationEffect(.degrees(self.rotation))
+                .animation(.easeInOut(duration: 5))
+                .foregroundColor(self.colors[self.colorIndex])
+            
+            Spacer()
+            Divider()
+            Slider(value: $rotation, in: 0 ... 360, step: 0.1)
+                .padding()
+            
+            TextField("Enter text here", text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Picker(selection: $colorIndex, label: Text("Color")) {
+                ForEach (0 ..< colornames.count) {
+                    Text(self.colornames[$0])
+                        .foregroundColor(self.colors[$0])
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
